1148211eafe9abbb4059d1f5fe92522d456d75b5
Higor Rocha <you@example.com>
2021-08-16 13:01:57 -0300

    Finishing the app

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,10 +12,21 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  String _price = "0";
+  String _priceBRL = "0";
+  String _priceEUR = "0";
+  String _priceUSA = "0";
 
-  void _recoverPrice() {
+  void _recoverPrice() async {
+    //String url = "https://blockchain.info/ticker";
+    Uri url = Uri.parse("https://blockchain.info/ticker");
+    http.Response response = await http.get(url);
 
+    Map<String, dynamic> retorn = json.decode(response.body);
+    setState(() {
+      _priceBRL = retorn["BRL"]["buy"].toString();
+      _priceEUR = retorn["EUR"]["buy"].toString();
+      _priceUSA = retorn["USD"]["buy"].toString();
+    });
   }
 
   @override
@@ -20,20 +34,45 @@ class _HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: Color(0xff558e9f),
       body: Container(
-        padding: EdgeInsets.all(32),
+        padding: EdgeInsets.all(15),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset("images/bitImage.png"),
               Padding(
-                  padding: EdgeInsets.only(top: 30, bottom: 30),
-                  child: Text(
-                    "R\$" + _price,
-                    style: TextStyle(
-                      fontSize: 35
-                    ),
-                  ),
+                  padding: EdgeInsets.only(top: 10, bottom: 10),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 3),
+                        child: Text(
+                          "R\$ " + _priceBRL,
+                          style: TextStyle(
+                              fontSize: 20
+                          ),
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(top: 3),
+                        child: Text(
+                          "€ " + _priceEUR,
+                          style: TextStyle(
+                              fontSize: 20
+                          ),
+                        ),
+                      ),
+                      Padding(
+                          padding: EdgeInsets.only(top: 3),
+                        child: Text(
+                          "\$ " + _priceUSA,
+                          style: TextStyle(
+                              fontSize: 20
+                          ),
+                        ),
+                      )
+                    ],
+                  )
               ),
               ElevatedButton(
                   onPressed: _recoverPrice,
@@ -46,7 +85,7 @@ class _HomeState extends State<Home> {
                   ),
                   style: ElevatedButton.styleFrom(
                     primary: Color(0xff0d1b36),
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20)
+                    padding: EdgeInsets.symmetric(horizontal: 25, vertical: 10)
                   ),
               )
             ],
